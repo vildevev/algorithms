bc690d349b2fa7b937cb62f4597370764aee5ed5
“Vilde <vilde@vevatne.no>
2018-06-07 10:37:55 -0700

    Solve reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+// 	return str
+// 		.split("")
+// 		.reverse()
+// 		.join("");
+// } runtime = O(n)
+
+// function reverse(str) {
+// 	let reversed = "";
+// 	for (let character of str) {
+// 		reversed = character + reversed;
+// 	}
+// 	return reversed;
+// } runtime = O(n)
+
+function reverse(str) {
+	return str.split("").reduce((reversed, character) => {
+		return character + reversed;
+	}, "");
+}
 
 module.exports = reverse;
